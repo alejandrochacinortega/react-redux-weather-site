06738b0f98cc31836316ab0887a82d63cc10363c
omar.chacin <omar.chacin@sigmagroup.no>
2017-01-30 15:17:45 +0100

    add react-sparklines

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
 
 
 class WeatherList extends React.Component {
   renderWeather(cityData) {
-    const name = cityData.city.name
+    const name  = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log('temps ', temps);
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red"/>
+          </Sparklines>
+        </td>
       </tr>
     )
   }
@@ -16,22 +24,22 @@ class WeatherList extends React.Component {
     return (
       <table className="table table-hover">
         <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
-          </tr>
+        <tr>
+          <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
+        </tr>
         </thead>
         <tbody>
-          {this.props.weather.map(this.renderWeather)}
+        {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
 
-function mapStateToProps({ weather }) {
+function mapStateToProps({weather}) {
   return {
     weather
   }
